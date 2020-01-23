5d82ed9b88cb8263c590b5f9d26636827a97a35d
Andres Molina <andresm@blanclabs.com>
2020-01-23 13:06:27 -0500

    Added env elements and lang support

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import { useTranslation } from 'react-i18next'
 import './App.css';
 
 const App: React.FC = () => {
+  const { t, i18n } = useTranslation();
   return (
     <div className="App">
+      <h1>{t('locations')}</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
